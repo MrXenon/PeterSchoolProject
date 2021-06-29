30d7a66d66ac9c6ae4edbe6dc97decf2c4305ab0
black <kschuit@student.scalda.nl>
2021-06-29 16:21:06 +0200

    tessa adde

--- assets/main/testimonial2.php
+++ assets/main/testimonial2.php
@@ -4,6 +4,7 @@
                 <div class="row align-items-center justify-content-center">
                     <div class="col-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 text-center text-md-left sm-margin-30px-bottom">
                     <h5><span style="background-color: #FFBD59; color:#fff;" class="heading">&nbsp;Natasja van der Velden&nbsp;</h5>
+                    <h6><span style="background-color: #FFBD59; color:#fff;" class="heading">&nbsp;20 jaar, Breda&nbsp;</h6>
                     <p>
                     <blockquote>
                     “Ik merk in mijn werk als onderwijsassistent op een middelbare school dat er veel selfies met filters worden gemaakt. 
@@ -29,19 +30,22 @@
             <div class="container">
                 <div class="row align-items-center justify-content-center">
                 <div class="col-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 text-center text-md-left sm-margin-30px-bottom">
-                    <img src='images/Natasja.png'>
+                    <img src='images/Tessa.jpg'>
                     </div>
                     <div class="col-6 col-xl-6 col-lg-6 col-md-6 col-sm-12 text-center text-md-left sm-margin-30px-bottom">
-                    <h5><span style="background-color: #FFBD59; color:#fff;" class="heading">&nbsp;Natasja van der Velden&nbsp;</h5>
+                    <h5><span style="background-color: #FFBD59; color:#fff;" class="heading">&nbsp;Tessa Snaphaan&nbsp;</h5>
+                    <h6><span style="background-color: #FFBD59; color:#fff;" class="heading">&nbsp;19 jaar, Waalwijk&nbsp;</h6>
                     <p>
                     <blockquote>
-                    “Ik merk in mijn werk als onderwijsassistent op een middelbare school dat er veel selfies met filters worden gemaakt. 
-                    De meiden maken bij hun selfies ook gebruik van beauty filters en zelf zie ik dat de selfies die met beauty filter gemaakt zijn steeds vaker worden 
-                    gebruikt als hun “echte” foto bij schoolopdrachten. Wanneer ik ze er op aanspreek of ze ook een foto hebben zonder beauty filter wordt er 
-                    meestal negatief op gereageerd. Ik heb gemerkt dat bij de meiden de groepsdruk erg hoog is en alle meiden willen zo knap mogelijk op hun foto staan 
-                    omdat ze bang zijn dat ze anders het doelwit van pestgedrag zullen worden. Ik steun Filters4Fun in hun doel om een watermerk op beauty filters te 
-                    zetten en om meer aandacht te krijgen over de negatieve gevolgen van het gebruik van beauty filters. 
-                    Teken de petitie en laten we samen filters fun houden.”
+                    “Toen ik op de middelbare school zat heb ik overwogen om plastische chirurgie te laten doen. 
+                    Ik was 16 en voelde me enorm ongelukkig over mijn uiterlijk. 
+                    De reden dat ik me zo ongelukkig voelde was door de hoeveelheid opmerkingen die ik kreeg van meiden uit de klas. 
+                    Als we met de klas foto’s gingen maken was ik bang voor het resultaat en het commentaar dat erop zou komen. 
+                    Alle foto’s die ik van mezelf maakte waren met filter en ook ging ik steeds vaker beauty filters gebruiken om er zelfverzekerder van te worden. 
+                    Alleen hadden deze beauty filters het tegenovergestelde effect. Ik durfde geen foto’s meer van mezelf te maken zonder beauty filter omdat ik mezelf niet mooi genoeg vond. 
+                    Pas nadat ik met de middelbare school klaar was begon ik zelfverzekerder te worden over mijn uiterlijk. 
+                    Tegenwoordig maak ik alleen nog maar foto’s met leuke filters en niet om er mooier uit te zien. 
+                    Daarom vraag ik aan iedereen om de petitie te ondertekenen en Filters4Fun te steunen zodat niemand dezelfde onzekerheid hoeft te voelen die ik had.”
                     </blockquote>
                     </p>    
                     </div>
